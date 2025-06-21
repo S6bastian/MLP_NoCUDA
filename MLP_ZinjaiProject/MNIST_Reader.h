02f74176f933ac5866b6e2c6345241f814e8400d
Sebastián Sánchez <109748087+S6bastian@users.noreply.github.com>
2025-06-20 19:20:50 -0500

    Delete MLP_ZinjaiProject directory

--- MLP_ZinjaiProject/MNIST_Reader.h
+++
@@ -1,8 +0,0 @@
-// MNIST_Reader.h
-#pragma once
-#include <vector>
-#include <string>
-#include <cstdint>
-
-std::vector<std::vector<uint8_t>> load_mnist_images(const std::string& path, int& num_images);
-std::vector<uint8_t> load_mnist_labels(const std::string& path, int& num_labels);
